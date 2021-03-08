7f8bfc4585abdf5efd853b2a5ec5e1370eb0de13
Laurentiu Marcut <laurentiu.marcut@form3.tech>
2021-03-08 15:43:23 +0100

    Add T doc

--- pkg/f1/testing/t.go
+++ pkg/f1/testing/t.go
@@ -13,6 +13,10 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// T is a type passed to Scenario functions to manage test state and support formatted test logs.
+// A test ends when its Scenario function returns or calls any of the methods FailNow, Fatal, Fatalf. 
+// Those methods must be called only from the goroutine running the Scenario function.
+// The other reporting methods, such as the variations of Log and Error, may be called simultaneously from multiple goroutines.
 type T struct {
 	// "iteration " + iteration number or "setup"
 	Iteration string
@@ -35,47 +39,61 @@ func NewT(iter, scenarioName string) (*T, func()) {
 	return t, t.teardown
 }
 
+// Name returns the name of the running Scenario.
 func (t *T) Name() string {
 	return t.Scenario
 }
 
+// FailNow marks the function as having failed and stops its execution by calling runtime.Goexit (which then runs all deferred calls in the current goroutine).
+// Execution will continue at the next Scenario iteration. FailNow must be called from the goroutine running the Scenario,
+// not from other goroutines created during the Scenario. Calling FailNow does not stop those other goroutines.
 func (t *T) FailNow() {
 	atomic.StoreInt64(&t.failed, int64(1))
 	runtime.Goexit()
 }
 
+// Fail marks the function as having failed but continues execution.
 func (t *T) Fail() {
 	atomic.StoreInt64(&t.failed, int64(1))
 }
 
+// Errorf is equivalent to Logf followed by Fail.
 func (t *T) Errorf(format string, args ...interface{}) {
 	t.Logf(format, args...)
 	t.Fail()
 }
 
+// Error is equivalent to Log followed by Fail.
 func (t *T) Error(err error) {
 	t.Logf("%s failed due to %s", t.Iteration, err.Error())
 	t.Fail()
 }
 
+// Fatalf is equivalent to Logf followed by FailNow.
 func (t *T) Fatalf(format string, args ...interface{}) {
 	t.Logf(format, args...)
 	t.FailNow()
 }
 
+// Fatal is equivalent to Log followed by FailNow.
 func (t *T) Fatal(err error) {
 	t.Logf("%s failed due to %s", t.Iteration, err.Error())
 	t.FailNow()
 }
 
+// Log formats its arguments using default formatting, analogous to Println, and records the text in the error log.
+// The text will be printed only if f1 is running in verbose mode.
 func (t *T) Log(args ...interface{}) {
 	t.Logger.Error(args...)
 }
 
+// Logf formats its arguments according to the format, analogous to Printf, and records the text in the error log.
+// A final newline is added if not provided. The text will be printed only if f1 is running in verbose mode.
 func (t *T) Logf(format string, args ...interface{}) {
 	t.Logger.Errorf(format, args...)
 }
 
+// Failed reports whether the function has failed.
 func (t *T) Failed() bool {
 	return atomic.LoadInt64(&t.failed) == int64(1)
 }
@@ -91,7 +109,8 @@ func recordTime(t *T, stageName string, start time.Time) {
 	metrics.Instance().Record(metrics.IterationResult, t.Scenario, stageName, metrics.Result(t.Failed()), time.Since(start).Nanoseconds())
 }
 
-// Cleanup registers a teardown function to be called when T has completed
+// Cleanup registers a function to be called when the scenario or the iteration completes.
+// Cleanup functions will be called in last added, first called order.
 func (t *T) Cleanup(f func()) {
 	t.teardownStack = append(t.teardownStack, f)
 }
