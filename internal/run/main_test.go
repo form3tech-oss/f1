b88c13c2afbbce1fdc96813f694838f3576b7eca
Nikolai Vladimirov <nikolay.vladimirov@form3.tech>
2024-03-10 20:36:07 +0200

    chore: simplify goleak check
    goleak provides `VerifyTestMain` for this use case

--- internal/run/main_test.go
+++ internal/run/main_test.go
@@ -4,19 +4,13 @@ import (
 	"os"
 	"testing"
 
-	log "github.com/sirupsen/logrus"
 	"go.uber.org/goleak"
 )
 
 func TestMain(m *testing.M) {
 	result := m.Run()
 
-	if result == 0 {
-		if err := goleak.Find(); err != nil {
-			log.Errorf("goleak: Errors on successful test run: %v\n", err)
-			result = 1
-		}
-	}
+	goleak.VerifyTestMain(m)
 
 	os.Exit(result)
 }
